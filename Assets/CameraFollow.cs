1533d643e7c637daeab7654d33e828fce6c95d6b
Julian <julianemoreno34@gmail.com>
2020-08-26 02:13:07 -0500

    Added 2 major updates
    Added a camera follow and added controls for the ship

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _objectToFollow = null;
+
+    Vector3 _objectOffset;
+
+
+    private void Awake()
+    {
+        //create an offset between this position and object's position 
+        _objectOffset = this.transform.position - _objectToFollow.position;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        this.transform.position = _objectToFollow.position + _objectOffset;
+    }
+}
